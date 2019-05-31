71670603de3fa47b75fd3e83eec698de090bdaf5
MicroDreamIT <shahanur700@gmail.com>
2019-06-01 03:49:08 +0600

    added node sass

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,13 @@ module.exports = {
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [
-                    'file-loader'
+                    {
+                        loader:'file-loader',
+                        options: {
+                            name:'[name].[ext]',
+                            outputPath:'fonts/'
+                        }
+                    }
                 ]
             },
 
